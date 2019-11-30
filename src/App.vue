f5221a598bd8c03cf8fcc6de95c206356b6416bf
Shivang <shiva.chaturvedi91@gmail.com>
2019-11-30 22:48:50 +0530

    Custom Directive with call back function implementation

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-customOn:click="onClick">Click Me</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        directives: {
+            customOn: {
+                bind(el, binding, vnode){
+                    const type= binding.arg;
+                    const fn = binding.value;
+                    el.addEventListener(type, fn);
+                }
+            }
+        },
+
+        methods: {
+            onClick: function(){
+                alert('Hello')
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
